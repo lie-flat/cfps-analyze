19054161e8a5fcbb86ef91988120d4a57d8f78cf
kxxt <rsworktech@outlook.com>
2021-10-31 13:49:56 +0800

    Add `primary`  and `key` in CFPS Shell.

--- process/cfps_shell.py
+++ process/cfps_shell.py
@@ -17,13 +17,40 @@ def set_global(key, value):
 def get_data_dir(year):
     return f"dataset/CFPS {year}"
 
-
+def get_primary_key(k,year):
+    if "adult" in k or 'child' in k or 'person' in k or 'famroster' in k:
+        return 'pid'
+    elif 'famecon' in k:
+        match year:
+            case 2016:
+                return 'fid16'
+            case 2018:
+                return 'fid18'
+            case 2014:
+                return 'fid14'
+            case 2012:
+                return 'fid12'
+            case 2010:
+                return 'fid'
+    elif 'family' in k:
+        return 'fid'
+    elif 'comm' in k:
+        match year:
+            case 2014:
+                return 'cid14'
+            case 2010:
+                return 'cid'
+    elif 'famconf' in k:
+        return 'pid', get_primary_key('famecon',year)
+    
 class StataDetail:
     def __init__(self, year, vk):
         self.year = year
         self.path = f"{get_data_dir(year)}/Data/Stata/cfps{year}{vk}.dta"
         self.schema = read_json(self.path + ".schemas.json")
+        self.key = vk
         self._data = None
+        self.primary = get_primary_key(vk, year)
 
     @property
     def data(self):
